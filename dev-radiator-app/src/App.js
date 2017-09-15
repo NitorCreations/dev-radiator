533adcdf38f61cba6d8fe1a9edab5b0862e1de4b
Jonas Berlin <jonas.berlin@nitorcreations.com>
2017-09-15 15:21:22 +0200

    Move Ping to separate file

--- dev-radiator-app/src/App.js
+++ dev-radiator-app/src/App.js
@@ -1,47 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-
-class Ping extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: "n/a",
-    };
-  }
-
-  componentDidMount() {
-    this.execute();
-  }
-
-  render() {
-    const na      = "n/a" === this.state.data;
-    const success = / \d+ received/.test(this.state.data);
-    return <div className="signal">
-      <p>
-        <span className={"signal " + (na ? "signal-unknown" : success ? "signal-green" : "signal-red")}/>
-        <span className="signal-label">{this.props.label}</span>
-      </p>
-      {!na && <pre className="onhover">{this.state.data}</pre>}
-    </div>;
-  }
-
-  execute() {
-    fetch('http://localhost:4333/ping', {
-      cache: 'no-cache',
-      method: 'POST',
-      body: JSON.stringify(
-        {
-          host: this.props.host,
-        }),
-    }).then(response => response.text())
-      .then(data => {
-        this.setState({data});
-        setTimeout(this.execute.bind(this), 5000);
-      }).catch(err => {
-      this.setState({data: "n/a"});
-    });
-  }
-}
+import Ping from './Ping';
 
 class App extends Component {
   render() {
